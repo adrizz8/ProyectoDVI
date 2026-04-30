d000d52a2598b52a071411f7445e6e1e396e92af
Azacabel <121041495+Azacabel@users.noreply.github.com>
2026-04-30 15:25:08 +0200

    entrada_dungeon trigger

--- src/Pasillo.js
+++ src/Pasillo.js
@@ -27,7 +27,7 @@ export default class Pasillo extends Phaser.Scene {
         entradas.set('desde_cafeteria_der', { x: 96, y: 300, direccion: 'right' });
         // Desde la puerta izquierda de la cafetería: aparece en el lado derecho del pasillo
         entradas.set('desde_cafeteria_izq', { x: map.widthInPixels - 96, y: 300, direccion: 'left' });
-        entradas.set('salida_mazmorra', { x: 610, y: 140, direccion: 'down' });
+        entradas.set('salida_mazmorra', { x: 620, y: 160, direccion: 'down' });
 
         this.physics.world.setBounds(
             0,
